0677d5e19206e72e2f229925932547144c2957ba
Pedro Lima <tsc.pedrolima@gmail.com>
2023-06-05 17:47:10 -0300

    feat add marker position and entered address function

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import './style/components/form.sass'
 import 'leaflet/dist/leaflet.css'
 
 import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import MarkerPosition from "./components/MarkerPosition";
 const apiKey = import.meta.env.VITE_API_KEY;
 
 import mobileBgImage from "./images/pattern-bg-mobile.png"
@@ -11,14 +12,18 @@ import iconArrow from "./images/icon-arrow.svg"
 
 function App() {
 
-  const [ address, setAddress ] = useState(null)
-  // const [ ipAddress, setIpAddress ] = useState("")
+  const [ address, setAddress ] = useState(null);
+  const [ ipAddress, setIpAddress ] = useState("");
+  const checkIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi
+  const checkDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/
   
   useEffect(() => {
     try {
       const getDataApi = async () => {
         const res = await fetch(
-          `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=8.8.8.8`
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=192.212.174.101`
         );
         const jsonRes = await res.json();
         setAddress(jsonRes);
@@ -32,15 +37,42 @@ function App() {
 
   }, [])
 
+  async function getEnteredAddress(){
+    const res = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&${
+        checkIpAddress.test(ipAddress) 
+        ? `ipAddress=${ipAddress}` 
+        : checkDomain.test(ipAddress) 
+        ? `domain=${ipAddress}` 
+        : ""
+      }`
+    );
+    const jsonRes = await res.json();
+    setAddress(jsonRes);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+
+    getEnteredAddress()
+  }
+
   return (
     <div className="App">
       <img className="background" src={mobileBgImage} alt="Background image" />
-
       {address && <>
         <section className="form">
           <h1 className='form_title'>IP Address Tracker</h1>
-          <form className='form_search'>
-            <input className='form_search_input' type="text" name="ipAddress" id="ipAddress" placeholder="Search for any IP address or domain" />
+          <form onSubmit={handleSubmit} autoComplete="off" className='form_search'>
+            <input 
+              className='form_search_input' 
+              type="text" 
+              name="ipAddress" 
+              id="ipAddress" 
+              value={ipAddress} 
+              onChange={(e) => setIpAddress(e.target.value)} 
+              placeholder="Search for any IP address or domain" 
+            />
             <button type="submit" className='form_search_icon'>
               <img src={iconArrow} alt="Icon arrow right" />
             </button>
@@ -70,11 +102,7 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[address.location.lat, address.location.lng]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+          <MarkerPosition address={address} />
         </MapContainer>
       </>}
     </div>
---
+++ src/components/MarkerPosition.jsx
@@ -0,0 +1,30 @@
+import { Marker, Popup, useMap } from 'react-leaflet';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+function MarkerPosition({ address }) {
+  const position = [address.location.lat, address.location.lng]
+  const map = useMap()
+
+  useEffect(() => {
+    map.flyTo(position, 13, {
+      animate: true,
+    })
+  }), [map, position]
+
+  return (
+    <>
+        <Marker position={position}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+    </>
+  )
+}
+
+MarkerPosition.propTypes = {
+  address: PropTypes.any
+}
+
+export default MarkerPosition;
